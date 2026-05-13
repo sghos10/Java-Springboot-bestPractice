51638ad51ea1597709c588748d24bd19d44129dd
sghos10 <89407186+sghos10@users.noreply.github.com>
2026-05-12 23:00:14 -0400

    Add endpoint to get products grouped by type

--- src/main/java/com/javatechie/controller/ProductController.java
+++ src/main/java/com/javatechie/controller/ProductController.java
@@ -111,5 +111,20 @@ public class ProductController {
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 
+     @GetMapping("/names")
+    public ResponseEntity<APIResponse> getProductsGroupByoption() {
+
+        Map<String, List<ProductResponseDTO>> products = productService.getProductsByTypes();
+        APIResponse<Map<String, List<ProductResponseDTO>>> responseDTO = APIResponse
+                .<Map<String, List<ProductResponseDTO>>>builder()
+                .status(SUCCESS)
+                .results(products)
+                .build();
+
+        log.info("ProductController::getProductsGroupByType by types  {}", ValueMapper.jsonAsString(responseDTO));
+
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
 
 }
